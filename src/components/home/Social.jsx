274e8a6b9de9a9fa428e7e1154d0fb3315f9c192
Kishore <109215974+Kishoresweety@users.noreply.github.com>
2025-09-09 21:40:06 +0530

    Update Social.jsx
    Link update

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/kishore.ly/" className="home__social-icon" target='_blank'>
+        <a href="https://www.instagram.com/easy.coder_?igsh=Y3NhZmlraTJnODA5" className="home__social-icon" target='_blank'>
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="https://github.com/Kishoresweety?tab=repositories" className="home__social-icon" target='_blank'>
+        <a href="https://github.com/Kishoresweety" className="home__social-icon" target='_blank'>
             <i className="uil uil-github-alt"></i>
         </a>
 
